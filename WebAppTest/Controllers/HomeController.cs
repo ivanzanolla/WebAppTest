5d446bab4f0d88811e04f52aab97b38be223027f
Ivan Zanolla <ivan.zanolla@gmail.com>
2021-02-12 09:00:00 +0100

    Add project files.

---
+++ WebAppTest/Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebAppTest.Models;
+
+namespace WebAppTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<User> users = new List<User>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44327/api/Users"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    users = JsonConvert.DeserializeObject<List<User>>(apiResponse);
+                }
+            }
+
+            return View(users);
+        }
+
+
+        public ViewResult GetUser() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            User user = new User();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44327/api/Users/" + id))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        user = JsonConvert.DeserializeObject<User>(apiResponse);
+                    }
+                    else
+                        ViewBag.StatusCode = response.StatusCode;
+                }
+            }
+            return View(user);
+        }
+
+        public ViewResult AddUser() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(User user)
+        {
+            User receivedUser = new User();
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("https://localhost:44327/api/Users", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    receivedUser = JsonConvert.DeserializeObject<User>(apiResponse);
+                }
+            }
+            return View(receivedUser);
+        }
+
+
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebAppTest/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppTest.Models
+{
+    public class User
+    {
+
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+    }
+}
